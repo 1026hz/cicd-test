50f6555e8960662251dfef73cc72c9caba25cd1b
hyup98 <hyup98@naver.com>
2025-05-05 01:12:46 +0900

    [FEAT] SecurityUtil에 Swagger관련 설정 추가

--- src/main/java/com/kakaobase/snsapp/global/security/SecurityUtil.java
+++ src/main/java/com/kakaobase/snsapp/global/security/SecurityUtil.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
