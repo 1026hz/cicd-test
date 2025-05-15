d96d32aac3a2497807e8069eacd9c42059e01a49
hyup98 <hyup98@naver.com>
2025-05-15 11:32:38 +0900

    [FIX] Application의 timeZone을 서울시간으로 설정

--- src/main/java/com/kakaobase/snsapp/Application.java
+++ src/main/java/com/kakaobase/snsapp/Application.java
@@ -3,10 +3,13 @@ package com.kakaobase.snsapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(Application.class, args);
     }
 
