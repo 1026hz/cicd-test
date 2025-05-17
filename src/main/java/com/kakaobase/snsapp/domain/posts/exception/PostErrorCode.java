499731baeb0b2e44d41d49d33bed28dbd5ba8095
hyup98 <hyup98@naver.com>
2025-05-17 22:41:11 +0900

    [FEAT] 유튜브 요약본에러에 Ai서버 관련 에러 추가

--- src/main/java/com/kakaobase/snsapp/domain/posts/exception/PostErrorCode.java
+++ src/main/java/com/kakaobase/snsapp/domain/posts/exception/PostErrorCode.java
@@ -20,12 +20,13 @@ public enum PostErrorCode implements BaseErrorCode {
     // 이미지 관련 에러
     INVALID_IMAGE_URL(HttpStatus.BAD_REQUEST, "invalid_image_url", "유효하지 않은 이미지 주소입니다.", "image_url"),
 
-    // 유튜브 관련 에러
+    // 유튜브 요약본 관련 에러
     INVALID_YOUTUBE_URL(HttpStatus.BAD_REQUEST, "invalid_format", "유튜브 링크가 유효하지 않습니다.", "youtube_url"),
     YOUTUBE_SUBTITLE_NOT_FOUND(HttpStatus.UNPROCESSABLE_ENTITY, "youtube_subtitle_not_found", "해당 유튜브 영상에 자막이 존재하지 않습니다.", "youtube_url"),
     UNSUPPORTED_SUBTITLE_LANGUAGE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "unsupported_subtitle_language", "해당 YouTube 영상의 자막이 한국어 또는 영어를 지원하지 않습니다", "youtube_url"),
     YOUTUBE_VIDEO_PRIVATE(HttpStatus.FORBIDDEN, "video_private", "해당 YouTube 영상은 비공개 동영상입니다.", "youtube_url"),
     YOUTUBE_VIDEO_NOT_FOUND(HttpStatus.NOT_FOUND, "video_not_found", "존재하지 않는 youtube url입니다.", "youtube_url"),
+    AI_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "ai_server_error", "서버에러로 youtube요약에 실패하였습니다", null),
 
     // 좋아요 관련 에러
     ALREADY_LIKED(HttpStatus.CONFLICT, "state_conflict", "이미 좋아요한 게시글입니다.", null),
