506cf22c60abbdc6a3f3fe1bc9a85cda030ae6fd
hyup98 <hyup98@naver.com>
2025-05-17 22:38:21 +0900

    [FIX] youtube요약상태에 알맞는 ErrorCode추가

--- src/main/java/com/kakaobase/snsapp/domain/posts/exception/YoutubeSummaryStatus.java
+++ src/main/java/com/kakaobase/snsapp/domain/posts/exception/YoutubeSummaryStatus.java
@@ -1,20 +1,26 @@
 package com.kakaobase.snsapp.domain.posts.exception;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 
+@Getter
 public enum YoutubeSummaryStatus {
-    INVALID_YOUTUBE_URL("invalid_format"),
-    YOUTUBE_SUBTITLE_NOT_FOUND("subtitles_not_found"),
-    UNSUPPORTED_SUBTITLE_LANGUAGE("unsupported_subtitle_language"),
-    YOUTUBE_VIDEO_PRIVATE("video_private"),
-    YOUTUBE_VIDEO_NOT_FOUND("video_not_found"),
-    AI_SERVER_FAILED("internal_server_error");
+    INVALID_YOUTUBE_URL("invalid_format", PostErrorCode.INVALID_YOUTUBE_URL),
+    YOUTUBE_SUBTITLE_NOT_FOUND("subtitles_not_found", PostErrorCode.YOUTUBE_SUBTITLE_NOT_FOUND),
+    UNSUPPORTED_SUBTITLE_LANGUAGE("unsupported_subtitle_language", PostErrorCode.UNSUPPORTED_SUBTITLE_LANGUAGE),
+    YOUTUBE_VIDEO_PRIVATE("video_private", PostErrorCode.YOUTUBE_VIDEO_PRIVATE),
+    YOUTUBE_VIDEO_NOT_FOUND("video_not_found", PostErrorCode.YOUTUBE_VIDEO_NOT_FOUND),
+    AI_SERVER_FAILED("internal_server_error", PostErrorCode.AI_SERVER_ERROR);
 
     private final String aiErrorCode;
+    private final PostErrorCode postErrorCode;
 
-    YoutubeSummaryStatus(String aiErrorCode) {
+    YoutubeSummaryStatus(String aiErrorCode, PostErrorCode postErrorCode) {
         this.aiErrorCode = aiErrorCode;
+        this.postErrorCode = postErrorCode;
     }
+
 
     // AI응답 error코드를 enum으로 매핑 메서드
     public static YoutubeSummaryStatus fromAiErrorCode(String aiErrorCode) {
