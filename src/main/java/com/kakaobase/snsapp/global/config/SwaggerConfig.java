fb3a04b31ad325f640e2d585f8cb741d9e9f44bd
1026hz <1026hzz@gmail.com>
2025-05-20 16:43:21 +0900

    [CHORE] SwaggerConfig Base URL 변경

--- src/main/java/com/kakaobase/snsapp/global/config/SwaggerConfig.java
+++ src/main/java/com/kakaobase/snsapp/global/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.kakaobase.snsapp.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +14,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        final String jwtSchemeName = "JWT TOKEN";
+
         return new OpenAPI()
-                .info(new Info().title("API 문서").version("v1"))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .addServersItem(new Server().url("/api"))
+                .info(new Info()
+                        .title("KakaoBase API")
+                        .description("KakaoBase의 API 명세서입니다.")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(jwtSchemeName))
                 .components(
-                        new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes("BearerAuth", new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                )
-                );
+                        new Components()
+                                .addSecuritySchemes(jwtSchemeName,
+                                        new SecurityScheme()
+                                                .name(jwtSchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")));
     }
 }
